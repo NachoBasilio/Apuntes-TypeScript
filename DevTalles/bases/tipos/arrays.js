b13b91325426ad6ef517f4a36d1b9734db5fd38b
IgnaDev <arcortoon@gmail.com>
2023-09-27 21:07:32 -0300

    arrays

---
+++ DevTalles/bases/tipos/arrays.js
@@ -0,0 +1,7 @@
+"use strict";
+(function () {
+    var numbers = [1, 2, 3, 4, 5];
+    var numbers2 = [1, 2, 3, 4, 5];
+    var strings = ['a', 'b', 'c', 'd', 'e'];
+    var strings2 = ['a', 'b', 'c', 'd', 'e'];
+})();
---
+++ DevTalles/bases/tipos/arrays.ts
@@ -0,0 +1,10 @@
+(()=>{
+
+    const numbers: number[] = [1,2,3,4,5];
+    const numbers2: Array<number> = [1,2,3,4,5];
+
+    const strings: string[] = ['a','b','c','d','e'];
+    const strings2: Array<string> = ['a','b','c','d','e'];
+
+
+})()
