322679a510b2d4fc157fcf33bed58c0743a7ae3a
IgnaDev <arcortoon@gmail.com>
2023-09-28 16:35:40 -0300

    tupla

--- DevTalles/bases/tipos/tuples.ts
+++ DevTalles/bases/tipos/tuples.ts
@@ -4,4 +4,6 @@
     hero[0] = 'Ironman';
     hero[1] = 50;
 
+    //Para typescript esto es un "tupla", pero en javascript es un arreglo. Lo que si hace TS es defender el tipo de dato que se le asigna a cada posición del arreglo.
+
 })()
