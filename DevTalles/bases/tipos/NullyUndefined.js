a5c1bf8809fc463a61f101de283d94cf05a8da92
IgnaDev <arcortoon@gmail.com>
2023-09-28 16:53:09 -0300

    NullyUndefined

---
+++ DevTalles/bases/tipos/NullyUndefined.js
@@ -0,0 +1,9 @@
+"use strict";
+(function () {
+    var nada = undefined;
+    var nulo = null;
+    var indefinido = undefined;
+    console.log(nada);
+    console.log(nulo);
+    console.log(indefinido);
+})();
---
+++ DevTalles/bases/tipos/NullyUndefined.ts
@@ -0,0 +1,10 @@
+(()=>{
+    let nada: undefined = undefined;
+    let nulo: null = null;
+    let indefinido: void = undefined;
+
+    console.log(nada);
+    console.log(nulo);
+    console.log(indefinido);
+
+})()
