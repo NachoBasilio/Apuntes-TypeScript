b33b755a7275d5fb86e0a3e2d9036792e7cbb465
IgnaDev <arcortoon@gmail.com>
2023-09-30 22:43:04 -0300

    Tipos

---
+++ DevTalles/bases/objetos/type.js
@@ -0,0 +1,14 @@
+"use strict";
+(function () {
+    var flash = {
+        nombre: 'Barry Allen',
+        edad: 24,
+        poderes: ['Puede correr muy rápido', 'Viajar en el tiempo']
+    };
+    var superman = {
+        nombre: 'Clark Kent',
+        edad: 60,
+        poderes: ['Puede volar', 'Super velocidad']
+    }; //Podemos definir esto de esta manera pero imaginemos que los dos objetos deben tener la misma firma, esto es un poco tedioso, para solucionar esto, podemos crear una interfaz
+    //En typescript cuando definimos un objetos, luego si queremos editar sus propiedades, no podemos hacerlo, ya que typescript lo toma como un objeto inmutable, para solucionar esto, debemos definir las propiedades como opcionales, para que luego podamos editarlas
+})();
---
+++ DevTalles/bases/objetos/type.ts
@@ -0,0 +1,23 @@
+(()=>{
+
+    type Hero = {
+        nombre:string,
+        edad?:number,
+        poderes:string[],
+        getNombre?: () => string
+    }//Definimos el tipo de dato que va a tener el objeto
+
+    let flash: Hero ={//Definimos el tipo de dato que va a tener el objeto, en este caso, del tipo Hero
+        nombre: 'Barry Allen',
+        edad: 24,
+        poderes: ['Puede correr muy rápido', 'Viajar en el tiempo']
+    }
+
+    let superman: Hero ={
+        nombre: 'Clark Kent',
+        edad: 60,
+        poderes: ['Puede volar', 'Super velocidad']
+    }//Podemos definir esto de esta manera pero imaginemos que los dos objetos deben tener la misma firma, esto es un poco tedioso, para solucionar esto, podemos crear una interfaz
+
+    //En typescript cuando definimos un objetos, luego si queremos editar sus propiedades, no podemos hacerlo, ya que typescript lo toma como un objeto inmutable, para solucionar esto, debemos definir las propiedades como opcionales, para que luego podamos editarlas
+})()
