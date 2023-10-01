b31ba09e2f96009bddb259874329ba8783a68f57
IgnaDev <arcortoon@gmail.com>
2023-09-30 22:38:50 -0300

    problema con firma de objetos

--- DevTalles/bases/objetos/objets.js
+++ DevTalles/bases/objetos/objets.js
@@ -5,13 +5,10 @@
         edad: 24,
         poderes: ['Puede correr muy rápido', 'Viajar en el tiempo']
     };
-    flash = {
+    var superman = {
         nombre: 'Clark Kent',
-        edad: 500,
-        poderes: ['Puede volar'],
-        getNombre: function () {
-            return this.nombre;
-        }
-    };
+        edad: 60,
+        poderes: ['Puede volar', 'Super velocidad']
+    }; //Podemos definir esto de esta manera pero imaginemos que los dos objetos deben tener la misma firma, esto es un poco tedioso, para solucionar esto, podemos crear una interfaz
     //En typescript cuando definimos un objetos, luego si queremos editar sus propiedades, no podemos hacerlo, ya que typescript lo toma como un objeto inmutable, para solucionar esto, debemos definir las propiedades como opcionales, para que luego podamos editarlas
 })();
--- DevTalles/bases/objetos/objets.ts
+++ DevTalles/bases/objetos/objets.ts
@@ -5,18 +5,11 @@
         poderes: ['Puede correr muy rápido', 'Viajar en el tiempo']
     }
 
-
-    
-
-    flash = {
+    let superman: {nombre:string, edad?:number, poderes:string[], getNombre?: () => string} ={//Definimos el tipo de dato que va a tener el objeto
         nombre: 'Clark Kent',
-        edad: 500,
-        poderes: ['Puede volar'],
-        getNombre(){
-             return this.nombre;
-         }
-    }
-
+        edad: 60,
+        poderes: ['Puede volar', 'Super velocidad']
+    }//Podemos definir esto de esta manera pero imaginemos que los dos objetos deben tener la misma firma, esto es un poco tedioso, para solucionar esto, podemos crear una interfaz
 
     //En typescript cuando definimos un objetos, luego si queremos editar sus propiedades, no podemos hacerlo, ya que typescript lo toma como un objeto inmutable, para solucionar esto, debemos definir las propiedades como opcionales, para que luego podamos editarlas
 })()
