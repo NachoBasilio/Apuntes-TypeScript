35984318d37f7df2800e44706b653a4e4f50afc5
IgnaDev <arcortoon@gmail.com>
2023-10-01 18:26:24 -0300

    Desestructuracion

---
+++ DevTalles/bases/ES6/desestructuracion.ts
@@ -0,0 +1,25 @@
+(()=>{
+    type RiverPlateJugadores = {
+        arquero: string,
+        defensa: string,
+        medio: string,
+        delantero: string
+    }
+
+    const riverPlateJugadores: RiverPlateJugadores = {
+        arquero: 'Armani',
+        defensa: 'Gonzales Pires',
+        medio: 'Enzo Perez',
+        delantero: 'Borja'
+    }
+
+    const {arquero, defensa, medio, delantero} = riverPlateJugadores;
+    //En JavaScript se puede desestructurar un objeto, es decir, extraer sus propiedades
+
+
+    const printPlayers = ({arquero, ...resto}:RiverPlateJugadores)=>{
+        console.log(arquero, resto)
+    }
+
+    printPlayers(riverPlateJugadores)
+})()
--- DevTalles/bases/main.js
+++ DevTalles/bases/main.js
@@ -1,4 +1,29 @@
 "use strict";
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
+(() => {
+    const riverPlateJugadores = {
+        arquero: 'Armani',
+        defensa: 'Gonzales Pires',
+        medio: 'Enzo Perez',
+        delantero: 'Borja'
+    };
+    const { arquero, defensa, medio, delantero } = riverPlateJugadores;
+    const printPlayers = (_a) => {
+        var { arquero } = _a, resto = __rest(_a, ["arquero"]);
+        console.log(arquero, resto);
+    };
+    printPlayers(riverPlateJugadores);
+})();
 (() => {
     let river = "campeon";
     console.log(river);
