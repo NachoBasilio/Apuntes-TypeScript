8ca4e25e2ed92441a29907fbc12ab46c05235c81
IgnaDev <arcortoon@gmail.com>
2023-09-30 23:05:51 -0300

    resolucion ejercicio

--- DevTalles/bases/app.ts
+++ DevTalles/bases/app.ts
@@ -1,32 +1,77 @@
-// Funciones Básicas
-function sumar( a:number, b:number ):number{
-  return a + b;
+
+// Objetos
+type autobot ={
+  carroceria:string,
+  modelo:string,
+  antibalas:boolean,
+  pasajeros:number,
+  disparar?:() => void // El metodo disparar es opcional
 }
 
-const contar = ( heroes:string[] ):number => {
-  return heroes.length;
-}
-const superHeroes = ["Flash", "Arrow", "Superman", "Linterna Verde"];
-contar(superHeroes);
+const batimovil: autobot = {
+  carroceria: "Negra",
+  modelo: "6x6",
+  antibalas: true,
+  pasajeros:4
+};
 
-//Parametros por defecto
-const llamarBatman = ( llamar:boolean = true ):void => {
-  if( llamar ){
-    console.log("Batiseñal activada");
+const bumblebee: autobot = {
+  carroceria: "Amarillo con negro",
+  modelo: "4x2",
+  antibalas: true,
+  pasajeros:4,
+  disparar(){ // El metodo disparar es opcional
+    console.log("Disparando");
   }
+};
+
+
+// Villanos debe de ser un arreglo de objetos personalizados
+
+type villanos = {
+  nombre:string,
+  edad: number | undefined,
+  mutante: boolean
 }
 
-llamarBatman();
+const villanos: villanos[] = [{
+  nombre:"Lex Luthor",
+  edad: 54,
+  mutante:false
+},{
+  nombre: "Erik Magnus Lehnsherr",
+  edad: 49,
+  mutante: true
+},{
+  nombre: "James Logan",
+  edad: undefined,
+  mutante: true
+}];
 
-// Rest?
-const unirheroes = ( ...personas:string[] ):string => {
-  return personas.join(", ");
+// Multiples tipos
+// cree dos tipos, uno para charles y otro para apocalipsis
+type Charles = {
+  poder: string,
+  estatura: number
 }
 
+type Apocalipsis = {
+  lider: boolean,
+  miembros: string[]
+}
 
-// Tipo funcion
-const noHaceNada = ( numero:number, texto:string, booleano:boolean, arreglo:any[] )=> {}
+const charles: Charles = {
+  poder:"psiquico",
+  estatura: 1.78
+};
 
-// Crear el tipo de funcion que acepte la funcion "noHaceNada"
-let noHaceNadaTampoco: (n:number, t:string, b:boolean, a:any[]) => void;
-noHaceNadaTampoco = noHaceNada
+const apocalipsis: Apocalipsis = {
+  lider:true,
+  miembros: ["Magneto","Tormenta","Psylocke","Angel"]
+}
+
+// Mystique, debe poder ser cualquiera de esos dos mutantes (charles o apocalipsis)
+let mystique: Charles | Apocalipsis;
+
+mystique = charles;
+mystique = apocalipsis;
