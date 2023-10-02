f22bb90ef371311c8c3e661baaa351ea539284d5
IgnaDev <arcortoon@gmail.com>
2023-10-02 20:02:22 -0300

    Forma rapida

--- DevTalles/bases/classes/basica.ts
+++ DevTalles/bases/classes/basica.ts
@@ -9,14 +9,9 @@
             public goals?: number,
             private club: string = 'River Plate'
         ){}
-
-        static createPlayer(name: string, age: number, position: string, goals: number){
-            return new RiverPlatePlayer(name, age, position, goals);
-        }
     }
 
     const player = new RiverPlatePlayer('Juanfer', 27, 'Midfielder', 3, "Racing");
     console.log(player);
 
-    RiverPlatePlayer.createPlayer('Borré', 25, 'Forward', 10);//Las propiedades Static solo se pueden acceder desde la clase
 })()
--- DevTalles/bases/main.js
+++ DevTalles/bases/main.js
@@ -1,60 +1,15 @@
 "use strict";
-var __rest = (this && this.__rest) || function (s, e) {
-    var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
-            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
-                t[p[i]] = s[p[i]];
+(() => {
+    class RiverPlatePlayer {
+        constructor(name, age, position, goals, club = 'River Plate') {
+            this.name = name;
+            this.age = age;
+            this.position = position;
+            this.goals = goals;
+            this.club = club;
         }
-    return t;
-};
-(() => {
-    const riverPlateJugadores = {
-        arquero: 'Armani',
-        defensa: 'Gonzales Pires',
-        medio: 'Enzo Perez',
-        delantero: 'Borja'
-    };
-    const { arquero, defensa, medio, delantero } = riverPlateJugadores;
-    const printPlayers = (_a) => {
-        var { arquero } = _a, resto = __rest(_a, ["arquero"]);
-        console.log(arquero, resto);
-    };
-    printPlayers(riverPlateJugadores);
-    const jugadoresArr = ['Armani', 'Gonzales Pires', 'Enzo Perez', 'Borja'];
-    const [arqueroArr, defensaArr, medioArr, delanteroArr] = jugadoresArr;
-    console.log(arqueroArr, defensaArr, medioArr, delanteroArr);
-})();
-(() => {
-    const pityMartinez = {
-        name: 'Gonzalo Pity Martinez',
-        salary: 1000000,
-        active: true
-    };
-    const juanferQuintero = {
-        name: 'Juan Fernando Quintero',
-        salary: 500000,
-        active: false
-    };
-    const enzoPerez = {
-        name: 'Enzo Perez',
-        salary: 800000,
-        active: true
-    };
-    const riverPlayers = [pityMartinez, juanferQuintero, enzoPerez];
-    for (const player of riverPlayers) {
-        console.log(player.name);
     }
-})();
-(() => {
-    let river = "campeon";
-    console.log(river);
-    const PityMartinez = "crack";
-    const riverVsBoca = () => {
-        console.log("Gano river 2 a 0, en la 'cancha' de boca, mejor dicho en la media cancha de boca");
-    };
-    riverVsBoca();
+    const player = new RiverPlatePlayer('Juanfer', 27, 'Midfielder', 3, "Racing");
+    console.log(player);
 })();
 //# sourceMappingURL=main.js.map
