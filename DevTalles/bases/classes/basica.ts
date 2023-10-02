c537cd37f90f9c305e3ab4a6d119449de559f828
IgnaDev <arcortoon@gmail.com>
2023-10-02 20:00:32 -0300

    Forma rapida

--- DevTalles/bases/classes/basica.ts
+++ DevTalles/bases/classes/basica.ts
@@ -1,23 +1,21 @@
 (()=>{
     class RiverPlatePlayer {
+
+        
         constructor(
             public name: string,
             public age: number,
             public position: string,
             public goals?: number,
-        ){
-            this.name = name;
-            this.age = age;
-            this.position = position;
-            this.goals = goals;
-        }
+            private club: string = 'River Plate'
+        ){}
 
         static createPlayer(name: string, age: number, position: string, goals: number){
             return new RiverPlatePlayer(name, age, position, goals);
         }
     }
 
-    const player = new RiverPlatePlayer('Juanfer', 27, 'Midfielder');
+    const player = new RiverPlatePlayer('Juanfer', 27, 'Midfielder', 3, "Racing");
     console.log(player);
 
     RiverPlatePlayer.createPlayer('Borré', 25, 'Forward', 10);//Las propiedades Static solo se pueden acceder desde la clase
